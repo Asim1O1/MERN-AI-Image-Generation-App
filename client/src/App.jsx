ea35edfc1b6ebcee69fe1a39f77a79e7d54acb30
Asim1O1 <146157042+Asim1O1@users.noreply.github.com>
2024-10-03 14:42:22 +0545

    app structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { logo } from "./assets";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1 className='text-3xl font-bold text-red-600'>
-      Hello World
-    </h1>
-
+      <BrowserRouter>
+        <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="Your Site Logo"
+              className="w-28 object-contain"
+            />
+          </Link>
+          <Link
+            to="/createPost"
+            className="font-inter font-medium bg-blue-600 text-white px-4 py-2 rounded-md"
+          >
+            Create
+          </Link>
+        </header>
+        <main className="sm:p-8 px-4 py-8 w-full bg-white min-h-[calc(100vh-73px)]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/createPost" element={<CreatePost />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
